f629b1f93550509fe4f73a8e1a51e4758b82f107
rrodriguezcs <90003077+rrodriguezcs@users.noreply.github.com>
2022-03-30 14:37:50 -0700

    skldjf

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\EquipmentController;
+use App\Http\Controllers\ManufacturerController;
+use App\Http\Controllers\NoteshistoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +18,9 @@ use App\Http\Controllers\CustomerController;
 */
 
 Route::resource('/customers', CustomerController::class);
+Route::resource('/equipment', EquipmentController::class);
+Route::resource('/manufacturer', ManufacturerController::class);
+Route::resource('/note', NoteshistoryController::class);
 
 Route::get('/', function () {
     return view('welcome');
