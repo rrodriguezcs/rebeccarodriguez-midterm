82a8664be5176e7da154314baf0ab0ed8f3b1d64
rrodriguezcs <90003077+rrodriguezcs@users.noreply.github.com>
2022-03-30 12:15:04 -0700

    customer

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::resource('/customer', CustomersController::class);
 
 Route::get('/', function () {
     return view('welcome');
