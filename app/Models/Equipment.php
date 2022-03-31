b38a8ec987bcc621920331a334b1189910a169d0
rrodriguezcs <90003077+rrodriguezcs@users.noreply.github.com>
2022-03-30 20:13:16 -0700

    equipmodel

---
+++ app/Models/Equipment.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Equipment extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name', 'price', 'ram', 'manufac_id','category'];
+
+    protected $table = 'equipment';
+
+    public function manufacturer(){
+
+        return $this ->belongsTo(Manufac::class);
+
+    }
+
+    public function notes()
+    {
+        return $this->hasMany(Notes::class);
+    }
+    public function invoice()
+    {
+        return $this->hasMany(Invoice::class);
+    }
+
+}
