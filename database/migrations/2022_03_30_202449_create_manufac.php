1e2c23e8ff31a01be63f7db8591843b55ab75575
rrodriguezcs <90003077+rrodriguezcs@users.noreply.github.com>
2022-03-30 20:20:23 -0700

    manufac

--- database/migrations/2022_03_30_202449_create_manufac.php
+++ database/migrations/2022_03_30_202449_create_manufac.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('manuinfo', function (Blueprint $table) {
+        Schema::create('manufac', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('name');
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('manuinfo');
+        Schema::dropIfExists('manufac');
     }
 };
